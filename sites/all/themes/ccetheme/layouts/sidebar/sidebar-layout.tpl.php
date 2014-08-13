b14e8e9398cfea14349b3f15b87e1abc246afd9b
tjp58 <tjp58@cornell.edu>
2014-08-13 13:49:33 -0400

    CCS Tweaks
    see above

--- sites/all/themes/ccetheme/layouts/sidebar/sidebar-layout.tpl.php
+++ sites/all/themes/ccetheme/layouts/sidebar/sidebar-layout.tpl.php
@@ -1,42 +1,44 @@
-<div class="l-page">
+<div<?php print $attributes; ?>>
   <header class="l-header" role="banner">
-    <div class="l-branding">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
+    <div class="l-constrained">
+      <div class="l-branding site-branding">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-branding__logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+        <?php endif; ?>
         <?php if ($site_name): ?>
-          <h1 class="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+          <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
         <?php endif; ?>
-
         <?php if ($site_slogan): ?>
-          <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
+          <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-      <?php endif; ?>
-
-      <?php print render($page['branding']); ?>
+        <?php print render($page['branding']); ?>
+      </div>
+      <?php print render($page['navigation']); ?>
+      <?php print render($page['header']); ?>
     </div>
-
-    <?php print render($page['header']); ?>
-    <?php print render($page['navigation']); ?>
   </header>
 
-  <div class="l-main">
-    <div class="l-content" role="main">
+  <?php print render($page['hero']); ?>
+
+  <?php if (!empty($page['highlighted'])): ?>
+    <div class="l-highlighted-wrapper">
       <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
+    </div>
+  <?php endif; ?>
+
+  <div class="l-main l-constrained">
+    <a id="main-content"></a>
+    <?php print render($tabs); ?>
+    <?php print $breadcrumb; ?>
+    <?php print $messages; ?>
+    <?php print render($page['help']); ?>
+
+    <div class="l-content" role="main">
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
@@ -45,9 +47,11 @@
     </div>
 
     <?php print render($page['sidebar_first']); ?>
+    <?php print render($page['sidebar_second']); ?>
   </div>
 
-  <footer class="l-footer" role="contentinfo">
+  <footer class="l-footer-wrapper" role="contentinfo">
     <?php print render($page['footer']); ?>
   </footer>
 </div>
+
